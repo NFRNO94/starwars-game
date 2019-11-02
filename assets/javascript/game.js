c23a0318ca8a73a819f6505f97b41ad93d7fa2fb
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-11-01 20:31:03 -0700

    instructions written, pseudo coding for logic, linked script file

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// constructor variable for game logic
+    //  select character function for user
+        //  hide character in selector area
+        //  show character under Your character span
+
+    //  select character function for ai
+        //  hide character in selector area
+        //  show character under AI character span
+
+    //  start game on attack click
+        //  assign characters a health value
+        //  assign characters an attack value
+        //  on attack click, reduce health per character accordingly
+        //  reflect on health display for character
+
+    //``defeat character function
+        //  when health of opponent reaches 0 or lower, remove player
+        //  display message to user showing ai character has lost, 
+            // choose a new character to defeat
+        //  allow user to select a new character to win
+
+    //  game win function
+        // display message showing user has won above reset button
+        // hide attack button
+
+    //  game loss function
+        // display message showing game is over
+        // hide attack button
+
+    // reset game on click of reset
+        // scores wiped and characters returned on page reload
+        // will add in select new character, and a restart function
+
+console.log("Hello World");
