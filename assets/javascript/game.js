20a960f2f799f1940ae605b2f5ea5cfb0cbeccfd
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-11-06 17:26:40 -0700

    game message, enemy render, update character functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@
 //  select character function for ai
 //  assign characters a health value
 //  assign characters an attack value
-
 //  start game on attack click
 //``defeat character function
 // choose a new character to defeat
@@ -27,28 +26,35 @@ function Character(name, title, image, hitPts, attackPts) {
                 + "\n---------------------------------\n");
             return true;
         }
-        console.log(this.name + " has died!");
+        console.log(this.name + " has been defeated!");
         return false;
     };
     this.attack = function (character2) {
         character2.hitPts -= this.attackPts
     };
-    // this.defeatOpp = function () {
-    // }
+    this.levelUp = function () {
+        this.hitPts += 200;
+    };
 };
 
+// object to store characters
 let characters = {};
 
 characters.darthMaul = new Character("Darth Maul", "Sith Lord", "assets/images/darthMaul3.jpg", 300, 50);
 characters.obiWan = new Character("Obi Wan Kenobi", "Jedi", "assets/images/obi-wan.png", 350, 35);
 characters.quiGonJinn = new Character("Qui-Gon Jinn", "Jedi", "assets/images/quigon.jpg", 250, 45);
 characters.darthVader = new Character("Darth Vader", "Sith Lord", "assets/images/darth-vader-2.jpg", 400, 40);
+// characters.rey = new Character("Rey", "Jedi", "assets/images/rey.jpg", 420, 30);
+// characters.kyloRen = new Character("Kylo Ren", "Sith Lord", "assets/images/kylo.jpg", 380, 35);
 
 console.log(characters);
-// darthMaul.printStats();
-// obiWan.printStats();
-// quiGonJinn.printStats();
-// darthVader.printStats();
+characters.darthMaul.printStats();
+characters.obiWan.printStats();
+characters.quiGonJinn.printStats();
+characters.darthVader.printStats();
+// characters.rey.printStats();
+// characters.kyloRen.printStats();
+
 
 // while (obiWan.isAlive() === true && darthMaul.isAlive() === true) {
 //     obiWan.attack(darthMaul);
@@ -58,37 +64,54 @@ console.log(characters);
 // }
 
 $(document).ready(function () {
+    let user;
+    let enemies = [];
+    let ai;
+    let turnCounter = 1;
+    let killCounter = 0;
 
-    // let attacker;
-    // let combatants = [];
-    // let defender;
-    // let turnCounter = 1;
-    // let killCount = 0;
-
-    // renderCharacter function to render character card
-    let renderCharacters = function (character, renderArea) {
+    // renderCharacter function to render character cards
+    let charRender = function (character, renderArea) {
         let cardDiv = $("<div class='card' data-name='" + character.name + "'>");
         let charName = $("<p class='character-name card-title text-center'>").text(character.name);
         let charImg = $("<img alt='image' class='character-img img-fluid'>").attr("src", character.image);
         let charHitPts = $("<h6 class='character-health text-center'>").text(character.hitPts);
         cardDiv.append(charName).append(charImg).append(charHitPts);
         $(renderArea).append(cardDiv);
-    }
-
-    // function to load all characters in character section
-    let initializeGame = function() {
-        for (let key in characters) {
-            renderCharacters(characters[key], "#characters-section");
-        }
     };
 
-    initializeGame();
+    // function to load all characters in character section
+    let initializeGame = function () {
+        for (let key in characters) {
+            charRender(characters[key], "#characters-section");
+        };
+    };
 
-    
+
+    let charUpdate = function (charObj, areaRender) {
+        $(areaRender).empty();
+        charRender(charObj, areaRender);
+    };
+
+    let enemyRender = function (enArr) {
+        // .hide for choose your caharacter subtitle
+        //.show for new subtitle for available to attack
+
+        // loop to go through available characters to render enemies
+        for (let i = 0; i < enArr.length; i++) {
+            charRender(enArr[i], "#characters-section")
+        };
+    };
+
+    let gameMessage = function(message) {
+        let messageTag = $("#game-message");
+        let newMessage = $("<div").text(message);
+        messageTag.append(newMessage)
+    };
 
     let resetGame = function (resultMessage) {
         // restart button variable
-        let restart = $("<button type='button' class='btn btn-danger btn-sm mb-1' id='reset'>Reset</button>").click(function () {
+        let restart = $("<button type='button' class='btn btn-danger btn-sm mb-1'>Reset</button>").click(function () {
             location.reload();
         });
 
@@ -97,7 +120,10 @@ $(document).ready(function () {
 
         //render the restart and victory message variable
         // $("body").append(gameState);
-        // $("body").append(restart);
+        $("#restart").append(restart);
     };
+
+    initializeGame();
+    resetGame();
 });
 
